472a4ffe928c74a35a0ccd7ac6401c519851622c
NikhilXavierJohn <nkh.john@gmail.com>
2018-03-30 11:13:24 +0530

    Add files via upload

--- Code/tri.py
+++ Code/tri.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from pydub import AudioSegment
 from pydub.silence import detect_nonsilent
 def findSound():
@@ -11,4 +12,19 @@ def findSound():
     for i in range(size):
         t1=a[i][0]
         t2=a[i][1]
+=======
+from pydub import AudioSegment
+from pydub.silence import detect_nonsilent
+def findSound():
+    sound = AudioSegment.from_wav("01_Course_Introduction_14-11.wav")
+    val=sound.dBFS
+    k=val-20
+    print(val)
+    print(len(sound))
+    a=(detect_nonsilent(sound,min_silence_len=3400, silence_thresh=k,seek_step=1))
+    size=len(a)
+    for i in range(size):
+        t1=a[i][0]
+        t2=a[i][1]
+>>>>>>> 1dcb99b2c04475e04f8ef00d33975d2e16a57f2d
         silencesplitter(t1,t2,i+1)
--- UI/app.py
+++ UI/app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import sys
 sys.path.append('C:/Users/nikhi/OneDrive/Documents/GitHub/Nefarians/Code')
 import Total as t
@@ -40,4 +41,48 @@ class AppWindow(QDialog):
 app = QApplication(sys.argv)
 w = AppWindow()
 w.show()
+=======
+import sys
+sys.path.append('C:/Users/nikhi/OneDrive/Documents/GitHub/Nefarians/Code')
+import Total as t
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox,QDialog,QFileDialog
+from ui import Ui_Form
+from PyQt5.uic import loadUi
+
+class AppWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.show()
+        self.ui.Browse.clicked.connect(self.getfile)
+        self.ui.transcribe.clicked.connect(self.tb)
+        self.ui.Download.clicked.connect(self.db)
+        
+        
+    def getfile(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","Video Files (*.mp4);;AudioFiles(*.mp3)", options=options)
+        self.ui.browsefile.setText(fileName)
+        self.ui.fileName=fileName
+    def tb(self):
+        s='.mp4'
+        print(self.ui.fileName)
+        if (self.ui.fileName.endswith(s)):
+             t.converter(self.ui.fileName)
+        elif(self.ui.fileName.endswith('.wav')):
+             t.splitter(self.ui.fileName)
+             
+    def db(self):
+         t.download(self.ui.Download.text())
+         self.s=loadUi('op.ui')
+         self.s.show() 
+        
+        
+
+app = QApplication(sys.argv)
+w = AppWindow()
+w.show()
+>>>>>>> 1dcb99b2c04475e04f8ef00d33975d2e16a57f2d
 sys.exit(app.exec_())
